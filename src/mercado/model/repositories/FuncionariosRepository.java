17d961491f976ca4c75379e8c6baa8ec9f1f3661
bernafan <bfmello@hotmail.com>
2016-07-11 17:59:26 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/mercado/model/entidade/Funcionario.java
    #	src/mercado/view/Main.java

--- src/mercado/model/repositories/FuncionariosRepository.java
+++ src/mercado/model/repositories/FuncionariosRepository.java
@@ -9,74 +9,43 @@ import mercado.model.entidade.Funcionario;
 public class FuncionariosRepository {
 
     // melhor orienta��o a objetos
-    private ArrayList<Funcionario> fr = new ArrayList();
+    private ArrayList<Funcionario> funcionarioRepository = new ArrayList();
 
     public void insere(Funcionario funcionario) {
-        fr.add(funcionario);
+        funcionarioRepository.add(funcionario);
     }
 
     public void remove(Funcionario funcionario) {
-        fr.remove(funcionario);
+       funcionarioRepository.remove(funcionario);
     }
-
-    //public Funcionario retornaFuncionario() {
-    //funcionarioRepository.
-    //}
-    /*
-	// pior orienta��o a objetos(repositorios separados)
-	private ArrayList<Gerente> gerenteRepository;
-	private ArrayList<Vendedor> vendedorRepository;
-	
-	public void insereNoVendedorRepository(Funcionario funcionario) {
-		Vendedor vendedor = (Vendedor) funcionario;
-		vendedorRepository.add(vendedor);
-	}
-
-	public void insereNoGerenteRepository(Funcionario funcionario) {
-		Gerente gerente = (Gerente) funcionario;
-		gerenteRepository.add(gerente);
-	}
-	
-     */
-    public void imprimeFuncionarios() {
-        System.out.println(fr);
-    }
-
-    public void setUsuarioAtual(Funcionario f) {
-        f.setStatus(true);
+    
+    public ArrayList getRepository() {
+        return this.funcionarioRepository;
     }
 
     //Função para verificar se o usuario e senha estao no bd
-    public boolean autenticaUsuario(Funcionario f) {
+    public boolean vereficaUsuarioNoRepositorio(Funcionario funcionario) {
         //Percorrendo o arrayList
-        for (int i = 0; i < fr.size(); i++) {
-
-            /* Pergunta Se o Id da posicao i de um arrayList fr é igual
+        for (int i = 0; i < funcionarioRepository.size(); i++) {
+             /* Pergunta Se o Id da posicao i de um arrayList fr é igual
                 ao Id passado pelo parametro.
                 Mesma coisa a senha.
              */
-            if (fr.get(i).getId().equals(f.getId()) && fr.get(i).getPassword().equals(f.getPassword())) {
-                System.out.println(fr);
+            if (funcionarioRepository.get(i).getId().equals(funcionario.getId()) 
+                    && funcionarioRepository.get(i).getPassword().equals(funcionario.getPassword())) {
+                System.out.println(funcionarioRepository);
                 return true;
-
             }
         }
         return false;
     }
 
-    public void login(Funcionario f) {
-        if (autenticaUsuario(f)) {
-            f.setStatus(true);
-            System.out.println("Usuario Logado! ");
-        } else {
-            System.out.println("Erro no login");
-        }
-    }
+    
 
     public void mostrarUsuarioAtual() {
-        for (int i = 0; i < fr.size(); i++) {
-            if (fr.get(i).getStatus()) {
-                System.out.println("Usuario Atual: "+fr.get(i).getId());
+        for (int i = 0; i < funcionarioRepository.size(); i++) {
+            if (funcionarioRepository.get(i).getStatus()) {
+                System.out.println("Usuario Atual: "+funcionarioRepository.get(i).getId());
             } else {
                 System.out.println("Nenhum usuario logado.");
 
