2a8b3b906a970bab792d2fde14902624d58a402b
bernafan <bfmello@hotmail.com>
2016-07-11 17:53:46 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/mercado/model/entidade/Funcionario.java
    #	src/mercado/view/Main.java

--- src/mercado/model/entidade/Funcionario.java
+++ src/mercado/model/entidade/Funcionario.java
@@ -1,23 +1,23 @@
 package mercado.model.entidade;
 
-public abstract class Funcionario {
-
-	protected String nome;
-	protected String id;
+public class Funcionario {
+    protected String nome;
+    protected String id;
     protected String password;
     protected boolean status;
 	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getId() {
-		return this.id;
-	}
+    public String getNome() {
+            return nome;
+    }
+
+    public void setNome(String nome) {
+            this.nome = nome;
+    }
+
+    public String getId() {
+            return this.id;
+    }
+    
     public String getPassword(){
         return this.password;
     }
@@ -29,8 +29,9 @@ public abstract class Funcionario {
     public boolean getStatus(){
         return status;
     }
-	public String toString() {
-        return "Nome: "+nome+" Id: "+id+" Senha: "+password+"\n";
-	}
+    
+    public String toString() {
+    return "Nome: "+nome+" Id: "+id +"\n";
+    }
 	
 }
